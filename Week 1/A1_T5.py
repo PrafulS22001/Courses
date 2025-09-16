794a8e7d4fdeab2bbea64cefdaf1d409f12c36c6
Praful Sharma <Praful.Sharma@student.lab.fi>
2025-09-16 12:24:02 +0300

    Update

---
+++ Week 1/A1_T5.py
@@ -0,0 +1,8 @@
+print ("Calculate the area of a wall.")
+
+Width = float(input("Enter the width in meters: "))
+Height = float(input("Enter the height in meters: "))
+
+print("Width is {0} m and height is {1} m.".format(int(Width), int(Height)))
+Area = int(Width * Height)
+print("The wall will be {0} square meters.".format(Area))
---
+++ Week 1/A1_T6.py
@@ -0,0 +1,8 @@
+# To calulate the MOD value when imput number is divisible by 2 
+
+feed = input ("Insert an integer: ")
+Value = int(feed) 
+
+Remainder = Value % 2
+print (f"Value is {Value}")
+print (f"The remainder is {Remainder} when {Value} is divided by 2.")
