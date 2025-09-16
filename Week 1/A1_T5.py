31b2e7f391e9de725835b00349eb7a0c7eb9ada0
Praful Sharma <Praful.Sharma@student.lab.fi>
2025-09-16 15:31:52 +0545

    Update A1_T5.py

--- Week 1/A1_T5.py
+++ Week 1/A1_T5.py
@@ -1,8 +1,12 @@
+# Calculate the area of a wall 
 print ("Calculate the area of a wall.")
 
-Width = float(input("Enter the width in meters: "))
-Height = float(input("Enter the height in meters: "))
+Feed= float(input("Enter the width in meters: "))
+Width = round(Feed) 
+Feed1 = float(input("Enter the height in meters: "))
+Height = round(Feed1)
 
+print("The info you entered are: ")
 print("Width is {0} m and height is {1} m.".format(int(Width), int(Height)))
 Area = int(Width * Height)
 print("The wall will be {0} square meters.".format(Area))
