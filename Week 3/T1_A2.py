2e71f2ac9490a5b3777f670f203868290cb86ac4
Praful Sharma <Praful.Sharma@student.lab.fi>
2025-09-25 10:58:40 +0300

    Update T1_A2.py

--- Week 3/T1_A2.py
+++ Week 3/T1_A2.py
@@ -12,9 +12,9 @@ else:
 
 Word2 = input ("Insert second word: ")
 
-if Word1 > Word2:
+if Word1 < Word2:
     print (f"The first word \"{Word1}\" is before the second word \"{Word2}\" in alphabetically.")
-elif Word1 < Word2:
+elif Word1 > Word2:
     print (f"The second word \"{Word2}\" is before the first word \"{Word1}\" in alphabetically.")
 else:
     print (f"Both inserted words are the same alphabetically, {Word1}\n")
